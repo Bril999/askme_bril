5fabeb454eb07637862da0503530fc26d495ca3a
bds22u599 <brildima1@gmail.com>
2024-11-12 17:09:03 +0300

    .gitignore, exe files

--- app/models.py
+++ app/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Sum, Count
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new_questions(self):
+        return self.order_by('-created_at')
+
+    def hot_questions(self):
+        return self.annotate(likes=Count('questionlike')).order_by('-likes')
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(null=True, blank=True)
+
+class Question(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField('Tag', related_name='questions', blank=True)
+
+    objects = QuestionManager()
+
+    def __str__(self):
+        return self.title
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class QuestionLike(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'question')
+
+class AnswerLike(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'answer')
